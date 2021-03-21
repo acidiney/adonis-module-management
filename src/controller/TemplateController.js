7a76ba36016dfd97a2956badd268263a1d3a3308
Acidiney Dias <acidineydias@gmail.com>
2021-03-21 20:57:09 +0100

    mock

--- src/controller/TemplateController.js
+++ src/controller/TemplateController.js
@@ -1,6 +1,9 @@
 const TemplateControllerBase = use('App/Controllers/Http/TemplateController');
 
 class TemplateController extends TemplateControllerBase {
+  index () {
+    return 200
+  }
 }
 
 module.exports = TemplateController;
